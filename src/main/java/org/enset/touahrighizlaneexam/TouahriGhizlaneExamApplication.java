fd455f0c8e0178ee660e567a4a57385ec34545b6
Ghizlanethr <touahrighizlane98@gmail.com>
2026-05-11 09:26:01 +0100

    Initialisation du projet Spring Boot

---
+++ src/main/java/org/enset/touahrighizlaneexam/TouahriGhizlaneExamApplication.java
@@ -0,0 +1,13 @@
+package org.enset.touahrighizlaneexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouahriGhizlaneExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouahriGhizlaneExamApplication.class, args);
+    }
+
+}
